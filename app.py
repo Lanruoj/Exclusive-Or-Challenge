4b3562857a5fb1746c3f4c2c19192781a1b02993
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-08-10 12:31:26 +1000

    returns XOR binary

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 def decimal_to_binary(decimal):
-	result = bin(decimal).replace("0b", "")
-	return result
+    result = bin(decimal).replace("0b", "")
+    return result
 
-if __name__ == "__main__":
-	binary = decimal_to_binary(23)
-	print(binary)
 
 def exclusive_or(num1, num2):
-	pass
+    bin_list1 = list(decimal_to_binary(num1))
+    bin_list2 = list(decimal_to_binary(num2))
+    result = []
+    i = 0
+    while len(bin_list1) < len(bin_list2):
+        bin_list1 = ["0"] + bin_list1
+    while len(bin_list2) < len(bin_list1):
+        bin_list2 = ["0"] + bin_list2
+
+    while i < len(bin_list1):        
+        if ((bin_list1[i]=="0") and (bin_list2[i]=="1")) or ((bin_list1[i]=="1") and (bin_list2[i]=="0")):
+            i += 1
+            result.append("1")
+        else:
+            i += 1
+            result.append("0")
+
+    return ("").join(result)
+
+
+if __name__ == "__main__":
+    bin_list = exclusive_or(3, 20)
+    print(bin_list)
